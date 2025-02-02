1040a622a6527ce3f53c659e472764a1eda30d3a
pcsishun <dekearthsa@hotmail.com>
2025-02-02 18:46:36 +0700

    first commit

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 const fs = require("fs");
-const readST = fs.createReadStream("./data.txt",
-    {
-        encoding: "utf-8",
-        highWaterMark: 8 * 1024
-    }
-);
-readST.on("data",(ck)=>{
-    console.log(Buffer.byteLength(ck, "utf-8") + "byte" + Buffer.byteLength(ck, "utf-8") / 1024 + "kb")
-    console.log("---------------------")
-    console.log(ck)
-    console.log("---------------------\n")
-})
+const readST = fs.createReadStream("./data.txt");
 
-readST.on("close",()=>{})
+readST.on("data", (ck) => {
+    console.log(Buffer.byteLength(ck, "utf-8") + "byte" + Buffer.byteLength(ck, "utf-8")/1024+"kb")
+    console.log("---------------")
+    console.log(ck)
+    console.log("---------------\n")
+});
